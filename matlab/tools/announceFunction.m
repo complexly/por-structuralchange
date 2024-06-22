1911ad2b787d2bdfbf23a5013dc45efde1834d1e
j-mcnerney <james.mcnerney@gmail.com>
2024-06-22 15:18:50 -0400

    First commit

---
+++ matlab/tools/announceFunction.m
@@ -0,0 +1,9 @@
+function announceFunction()
+% Output name of the function that this function has been placed in
+
+functionStack      = dbstack(1);
+callerFunctionName = functionStack.name;
+
+disp( [newline,'--------------------------------------------------------------------'] )
+disp(['RUNNING ',callerFunctionName,'...'])
+disp( ['--------------------------------------------------------------------'] )
