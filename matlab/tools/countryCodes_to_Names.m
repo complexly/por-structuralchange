a4a3fdcdab3a6994855c432b674e0867b667fdeb
j-mcnerney <james.mcnerney@gmail.com>
2024-06-27 13:47:01 -0400

    Move Code2Country.mat to data folder

--- matlab/tools/countryCodes_to_Names.m
+++ matlab/tools/countryCodes_to_Names.m
@@ -3,8 +3,8 @@ function countryName = countryCodes_to_Names(codeList)
 global pp
 
 % Used data importer GUI to go from a csv file to a .mat file
-%save(fullfile(pp.saveFolder, 'Code2Country.mat'), 'Code2Country')
-load(fullfile(pp.saveFolder, 'Code2Country.mat'))
+%save(fullfile(pp.dataFolder, 'Code2Country.mat'), 'Code2Country')
+load(fullfile(pp.dataFolder, 'Code2Country.mat'))
 
 countryName = table2array( Code2Country( ismember(Code2Country.code, codeList), 'countryName' ) );
 
